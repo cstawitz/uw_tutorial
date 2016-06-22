d7cc91b6dac12367ff168794275d13f18d4b5425
Christine Stawitz <cstawitz@uw.edu>
2016-06-22 13:50:08 -0700

    Christine (#2)
    * Revert "Revert "Add lecture 17 R code""
    
    This reverts commit 941968842206dc8445427a8ee7966fed9aaf5b96.
    
    * delete some stuff

---
+++ lect17.R
@@ -0,0 +1,6 @@
+shell("echo Hello World")
+shell("dir")
+shell("notepad.exe", wait=F)
+shell("rgui.exe")
+aa<-shell("dir", intern=T)
+file.copy("C:/Users/Christine/Documents/Dropbox/FISH559/Lecture17/lect17", "C:/Users/Christine/Documents/Dropbox/FISH559/Lecture17/lect17a")
